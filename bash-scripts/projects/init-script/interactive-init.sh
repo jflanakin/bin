bbaef296e90bfb43816032d3b039d8e70af48a5f
jflanakin <jflanakin+github@mail.rubyraccoon.net>
2022-08-17 21:41:42 -0500

    code fixes
    Silly little apostrophes got me again

--- bash-scripts/projects/init-script/interactive-init.sh
+++ bash-scripts/projects/init-script/interactive-init.sh
@@ -16,7 +16,7 @@ main() {
 	else
 		USERNAME=""
 		echo "This script will install necessary software as well as create a user with sudo privileges or give your existing user sudo privileges."
-		read -s "What is your username? (If your user doesn't exist yet, what username would you like to use?)
+		read -s "What is your username? (If your user does not exist yet, what username would you like to use?)
 	" USERNAME
 		
 		install_essential
